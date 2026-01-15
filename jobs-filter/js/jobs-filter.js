db27dcf201ba4b49a5f706412f3f4f798b139252
taxdoo2023 <134045858+taxdoo2023@users.noreply.github.com>
2026-01-15 16:16:56 +0200

    Job filter

---
+++ jobs-filter/js/jobs-filter.js
@@ -0,0 +1,49 @@
+      jQuery(document).ready(function($) {
+        let currentTech = '';
+        let currentPage = 1;
+
+        function showLoader(show = true) {
+          if (show) {
+            $('#job-loader').show();
+          } else {
+            $('#job-loader').hide();
+          }
+        }
+
+        function loadJobs(tech = '', page = 1) {
+          showLoader(true);
+
+          $.ajax({
+            url: jobs_ajax.ajax_url,
+            type: 'POST',
+            data: {
+              action: 'filter_jobs',
+              technology: tech,
+              page: page,
+            },
+            success: function(res) {
+              $('#jobs-list').html(res.jobs_html);
+              $('#pagination-container').html(res.pagination_html);
+              showLoader(false);
+            },
+            error: function() {
+              alert('Error loading jobs.');
+              showLoader(false);
+            }
+          });
+        }
+
+        $('#job-technology').on('change', function () {
+          currentTech = $(this).val();
+          currentPage = 1;
+          loadJobs(currentTech, currentPage);
+        });
+
+        $(document).on('click', '.ajax-page-link', function (e) {
+          e.preventDefault();
+          currentPage = $(this).data('page');
+          loadJobs(currentTech, currentPage);
+        });
+
+        loadJobs();
+      });
